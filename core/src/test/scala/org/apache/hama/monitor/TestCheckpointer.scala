5c3bc776412207bea89315048b7dd7b2bc3c9d6b
test <test@gmail.com>
2014-09-13 10:09:42 +0800

    cleanup comment.

--- core/src/test/scala/org/apache/hama/monitor/TestCheckpointer.scala
+++ core/src/test/scala/org/apache/hama/monitor/TestCheckpointer.scala
@@ -33,21 +33,13 @@ import org.apache.hama.message.CombinerUtil
 import org.apache.hama.message.compress.BSPMessageCompressor
 //import org.apache.hama.message.OutgoingMessageManager
 import org.apache.hama.message.Peer
-import org.apache.hama.ProxyInfo
+//import org.apache.hama.ProxyInfo
 import org.apache.hama.TestEnv
 import org.apache.hama.util.JobUtil
 import org.apache.hama.zk.LocalZooKeeper
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import scala.collection.JavaConversions._
-
-/*
-final case class PeerAndMessages[M <: Writable](ckptPath: String, 
-                                                peer: ProxyInfo,
-                                                msg: M)
-final case class NameAndMap(ckptPath: String, className: String, 
-                            variables: Map[String, String])
-*/
 
 final case class Msgs(arg1: Int, arg2: Int, arg3: Int)
 
@@ -121,34 +113,9 @@ class MockSuperstep(superstepCount: Long) extends Superstep {
 @RunWith(classOf[JUnitRunner])
 class TestCheckpointer extends TestEnv("TestCheckpointer") with LocalZooKeeper 
                                                            with JobUtil {
-/*
-  val rootPath = "/bsp/checkpoint"
-*/
   val superstepCount: Long = 1654
   val threshold = BSPMessageCompressor.threshold(Option(testConfiguration))
-/*
-  val jobIdentifier = "test"
-  val id = 9
-  val jobId = createJobId(jobIdentifier, id).toString
-*/
   val taskAttemptId = createTaskAttemptId("test", 9, 3, 2).toString
-/*
-  val path = "%s/%s/%s/%s".format(rootPath, jobId, superstepCount, 
-                                  taskAttemptId)
-  val ckptPath = path+".ckpt"
-  val zkCkptPath = path+".ok"
-
-  val peer1 = Peer.at("testActorSystem@host14:9274")
-  val msg1 = new Text("Book: A Report on the Banality of Evil")
-  val peer2 = Peer.at("testActorSystem@host35:8245")
-  val msg2 = new Text("The wanderer")
-  val peer3 = Peer.at("testActorSystem@local:33219")
-  val msg3 = new Text("Programming Erlang")
-
-  val first = PeerAndMessages(ckptPath, peer1, msg1)
-  val second = PeerAndMessages(ckptPath, peer2, msg2)
-  val third = PeerAndMessages(ckptPath, peer3, msg3)
-*/
 
   override protected def beforeAll = launchZk
 
@@ -157,26 +124,12 @@ class TestCheckpointer extends TestEnv("TestCheckpointer") with LocalZooKeeper
     super.afterAll
   }
 
-/*
-  def putVariables(superstep: Superstep, key: String, value: Writable): 
-      Superstep = {
-    superstep.collect(key, value)
-    superstep
-  }
-*/
-
   def messages(): List[Writable] = List[Writable](new IntWritable(192), 
                                                   new IntWritable(112), 
                                                   new IntWritable(23))
 
   it("test checkpointer.") {
     val superstep = new MockSuperstep(superstepCount)
-/*
-    putVariables(superstep, "count", new LongWritable(superstepCount))
-    putVariables(superstep, "flag", new BooleanWritable(true))
-    LOG.info("Test checkpointer for task attempt id "+taskAttemptId+
-             " at superstep "+superstepCount) 
-*/
     // no specific setting for task, so use testConfiguration instead.
     val ckpt = createWithArgs("checkpointer", classOf[MockCheckpointer], 
                               testConfiguration, testConfiguration, 
@@ -191,32 +144,6 @@ class TestCheckpointer extends TestEnv("TestCheckpointer") with LocalZooKeeper
     expect(23)
     expect(MockCheckpointer.tmpRootPath+
            "/job_test_0009/1654/attempt_test_0009_000003_2.ok")
-/*
-    val outgoing = OutgoingMessageManager.get[Writable](testConfiguration)
-    outgoing.addMessage(peer1, msg1)
-    outgoing.addMessage(peer2, msg2)
-    outgoing.addMessage(peer3, msg3)
-
-    asScalaIterator(outgoing.getBundleIterator).foreach ( entry => {
-      val peer = entry.getKey
-      val bundle = entry.getValue
-      ckpt ! SavePeerMessages[Writable](peer, bundle)
-    })
-
-    // verify peer/ bundle 
-    expectAnyOf(first, second, third)
-    expectAnyOf(first, second, third)
-    expectAnyOf(first, second, third)
-
-    val className = superstep.getClass.getName
-    val variables = superstep.getVariables
-    ckpt ! SaveSuperstep(className, variables)
-    // verify superstep, etc.
-    expectAnyOf(NameAndMap(ckptPath, className, variables.mapValues { v =>
-      v.toString
-    }))
-    expect(zkCkptPath)
-*/
     LOG.info("Done TestCheckpointer test case!")
   }
 }
